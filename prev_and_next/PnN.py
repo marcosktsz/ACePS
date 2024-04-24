438fea486e22e92747d4e0a263259c20aefcdbba
Marco Caceffo <ultimatemal2k@gmail.com>
2024-04-24 20:14:50 +0200

    update

--- prev_and_next/PnN.py
+++ prev_and_next/PnN.py
@@ -1,16 +1,7 @@
-tilings = []
+import sys
+
+sys.setrecursionlimit(100000)
 pairs = []
-counts = []
-
-
-def tiling(size, history=""):
-    if size == 0:
-        tilings.append(history)
-    elif size == 1:
-        tiling(size - 1, history + "[]")
-    else:
-        tiling(size - 1, history + "[]")
-        tiling(size - 2, history + "[--]")
 
 
 def parentheses(size, history="", left=0, right=0):
@@ -22,13 +13,75 @@ def parentheses(size, history="", left=0, right=0):
         parentheses(size, history + ")", left, right + 1)
 
 
-def numbers(size, base, history=""):
-    if size == 0:
-        counts.append(history)
-    else:
-        for n in range(base):
-            numbers(size - 1, base, history + str(n))
-            # numbers(size - 1, base, history + "0")
+def tilingPrev(string):
+    k = len(string)
+
+    if k <= 0:
+        return ""
+    next = string[:]
+    while k:
+        if next[k - 4 : k] == "[--]":
+            next = next[: k - 4] + "[][]" + next[k:]
+            break
+        if next[k - 6 : k] == "[--][]":
+            next = next[: k - 6] + "[][--]" + next[k:]
+            break
+        else:
+            next = tilingPrev(string[: k - 4]) + "[--]" + next[k:]
+            break
+    if len(next) != len(string):
+        return ""
+    return next
+
+
+def tilingNext(string):
+    k = len(string)
+    if k == 0:
+        return ""
+    next = string[:]
+    while k:
+        if next[k - 4 : k] == "[][]":
+            next = next[: k - 4] + "[--]" + next[k:]
+            break
+        if next[k - 4 : k] == "[--]":
+            if next[k - 6 : k - 4] == "-]":
+                next = tilingNext(string[: k - 4]) + "[][]" + next[k:]
+                break
+            else:
+                next = next[: k - 6] + "[--][]" + next[k:]
+            break
+        else:
+            k -= 2
+
+    if len(next) != len(string):
+        return ""
+    return next
+
+
+def numbers(size, base, number):
+    k = size - 1
+    j = size - 1
+    prev = number[:]
+    while prev:
+        if prev[k] > 0:
+            prev[k] -= 1
+            break
+        else:
+            prev[k] = base - 1
+            k -= 1
+            if k < 0:
+                print()
+                break
+    print(" ".join(map(str, prev)))
+    next = number[:]
+    while next:
+        if next[j] < base - 1:
+            next[j] += 1
+            break
+        else:
+            next[j] = 0
+            j -= 1
+    print(" ".join(map(str, next)))
 
 
 T = int(input())
@@ -37,18 +90,9 @@ for _ in range(T):
     string = input()
     if "[" in string:
         size = int(len(string) / 2)
-        tiling(size)
-        for index, tiles in enumerate(tilings):
-            if tiles == string:
-                if index > 1:
-                    print(tilings[index - 1])
-                else:
-                    print()
-                if index < len(tilings) - 1:
-                    print(tilings[index + 1])
-                else:
-                    print()
-        tilings = []
+        print(tilingPrev(string))
+        print(tilingNext(string))
+
     elif "(" in string:
         size = int(len(string) / 2)
         parentheses(size)
@@ -64,5 +108,8 @@ for _ in range(T):
                     print()
         pairs = []
     else:
-        numbers(3, 6)
-        print(counts)
+        c = list(map(int, string.split()))
+        base = max(c) + 1
+        size = len(c)
+        numbers(size, base, c)
+        counts = []
